3dbdeb8ec29930f0c6a6bd20fa7f84f6dee32045
muhammadTanver <Muhammadtanveertaqi@gmail.com>
2024-10-31 22:56:13 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       fontFamily: {
         "Source": ["Source Sans 3", "sans-serif"],
-    
+
+      },
+      backgroundImage: {
+        'image1': "url('../src/main.jpg')",
+        'image2': "url('../src/dhamtour.png')",
+        'image3': "url('../src/main1.jpg')",
       },
     },
   },
