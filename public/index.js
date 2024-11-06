ce872db139d86d5a117aaf89b3543793d34b5be4
muhammadTanver <Muhammadtanveertaqi@gmail.com>
2024-11-07 01:01:56 +0500

    .

--- public/index.js
+++ public/index.js
@@ -1,45 +1,44 @@
-let button = document.querySelector(".button");
-let buttonclass = document.querySelector(".buttonclass");
-let mobileNav = document.querySelector(".mobile");
-let temp = 0;
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
 
-button.addEventListener("click", () => {
-    temp++;
-    if (temp % 2 != 1) {
 
-        mobileNav.classList.replace("flex", "hidden");
+// AOS.init({
+//     offset: 120, // Offset from the original trigger point
+//     duration: 800, // Animation duration in milliseconds
+//     easing: 'ease-in-out', // Easing function for the animation
+//     once: true, // Whether animation should happen only once
+//     mirror: false, // Whether elements should animate out when scrolling past them
+// });
 
-        buttonclass.classList.replace("fa-close", "fa-bars");
+document.addEventListener('DOMContentLoaded', () => {
+    let button = document.querySelector(".button");
+    let buttonclass = document.querySelector(".buttonclass");
+    let mobileNav = document.querySelector(".mobile");
+    let temp = 0;
 
+    button.addEventListener("click", () => {
+        temp++;
+        if (temp % 2 != 1) {
+            mobileNav.classList.replace("flex", "hidden");
+            buttonclass.classList.replace("fa-close", "fa-bars");
+        } else {
+            buttonclass.classList.replace("fa-bars", "fa-close");
+            mobileNav.classList.replace("hidden", "flex");
+            mobileNav.classList.add("delay-400");
+            mobileNav.classList.add("animate-slideDown");
+        }
+    });
+
+    const background = document.getElementById("background");
+    const images = ["bg-image1", "bg-image2", "bg-image3"];
+    let currentIndex = 0;
+
+    function changeBackground() {
+        background.classList.remove(images[currentIndex]);
+        currentIndex = (currentIndex + 1) % images.length;
+        background.classList.add(images[currentIndex]);
     }
-    else {
 
-        buttonclass.classList.replace("fa-bars", "fa-close");
-        mobileNav.classList.replace("hidden", "flex");
-
-        mobileNav.classList.add("delay-400");
-        mobileNav.classList.add("animate-slideDown");
-
-
-    }
-
+    setInterval(changeBackground, 4000);
 });
 
-
-// 
-
-const background = document.getElementById("background");
-const images = ["bg-image1", "bg-image2", "bg-image3"];
-let currentIndex = 0;
-
-function changeBackground() {
-
-    background.classList.remove(images[currentIndex]);
-
-    currentIndex = (currentIndex + 1) % images.length;
-
-    background.classList.add(images[currentIndex]);
-}
-
-
-setInterval(changeBackground, 4000);
